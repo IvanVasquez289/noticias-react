144de234c75fa7adcbe90a4b254153368a521834
Ivan Enrique Vasquez Ramirez <ivanvasquez@Ivans-MacBook-Air.local>
2023-06-01 00:14:03 -0600

    Detectando la categoria seleccionada

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,5 +1,5 @@
 import { FormControl, InputLabel, Select, MenuItem, Button, Box} from "@mui/material"
-
+import useNoticias from "../hooks/useNoticias"
 const CATEGORIAS = [
     { value: 'general', label: 'General'},
     { value: 'business', label: 'Negocios'},
@@ -11,12 +11,17 @@ const CATEGORIAS = [
 ]
 
 const Formulario = () => {
+
+  const {categoria, handleChangeCategoria} = useNoticias()
+
   return (
     <form>
         <FormControl fullWidth>
             <InputLabel>Categoria</InputLabel>
             <Select 
                 label='Categoria'
+                onChange={ e => handleChangeCategoria(e)}
+                value={categoria}
             >
                 {CATEGORIAS.map( categoria => (
                     <MenuItem 
