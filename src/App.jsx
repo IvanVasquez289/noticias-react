708835df9beb72f61fcbc26abd5cbb9219ac1653
Ivan Enrique Vasquez Ramirez <ivanvasquez@Ivans-MacBook-Air.local>
2023-05-31 23:57:18 -0600

    Creando el Context

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,29 @@
 import { Container , Typography, Grid} from '@mui/material';
 import Formulario from './components/Formulario';
+import { NoticiasProvider } from './context/NoticiasProvider';
+
 function App() {
   return (
-    <Container>
-        <header>
-          <Typography align='center' marginY={5} component={'h1'} variant='h3'>
-            Buscador de noticias
-          </Typography>
-        </header>
+    <NoticiasProvider>
+      <Container>
+          <header>
+            <Typography align='center' marginY={5} component={'h1'} variant='h3'>
+              Buscador de noticias
+            </Typography>
+          </header>
 
-        <Grid 
-          container
-          justifyContent={'center'}
-          alignItems={'center'}
-          direction={'row'}
-        >
-          <Grid item xs={12} md={6} lg={4}>
-            <Formulario/>
+          <Grid 
+            container
+            justifyContent={'center'}
+            alignItems={'center'}
+            direction={'row'}
+          >
+            <Grid item xs={12} md={6} lg={4}>
+              <Formulario/>
+            </Grid>
           </Grid>
-        </Grid>
-    </Container>
+      </Container>
+    </NoticiasProvider>
   ) 
 }
 
